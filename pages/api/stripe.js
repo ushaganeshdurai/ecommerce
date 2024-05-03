5e4bdc8c18110fc922d010b66a7b923c8d4a9751
ushaganeshdurai <gushanandhini2004@gmail.com>
2024-05-03 16:59:04 +0530

    shipping rates added

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -3,18 +3,30 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-        const params = {
-            line_items: [
-              {
-                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell //2:44
-                price: '{{PRICE_ID}}',
-                quantity: 1,
-              },
-            ],
-            mode: 'payment',
-            success_url: `${req.headers.origin}/?success=true`,
-            cancel_url: `${req.headers.origin}/?canceled=true`,
+      const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_options: [
+          {
+            shipping_rate:'shr_1PCKEoSAtO73Z9Ae9GfyBV7L'
+          },
+          {
+            shipping_rate:'shr_1PCKGDSAtO73Z9AeUj6RsLnJ'
           }
+        ],
+            line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell //2:44
+            price: '{{PRICE_ID}}',
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      }
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
       res.redirect(303, session.url);
