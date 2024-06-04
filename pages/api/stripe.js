d8e0280c785ab7aabd3d74f8c5991b7d50cbfea5
ushaganeshdurai <gushanandhini2004@gmail.com>
2024-06-04 17:05:36 +0530

    images

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
           const newImage = img.replace('image-', 'https://cdn.sanity.io/images/qs2hb0re/production/').replace('-webp', '.webp');
-
+          console.log('IMAGE',newImage);
           return {
             price_data: { 
               currency: 'inr',
